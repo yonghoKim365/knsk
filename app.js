969a6fb31e05bf0293bd31aa77539fc5983162d0
ChanBaek7th <7thbaekchan@gmail.com>
2022-09-06 17:05:37 +0900

    check logic

--- app.js
+++ app.js
@@ -10,17 +10,25 @@ const day = today.getDay();
 const date_str = month + "월 " + date + "일" + "(" + week[day] + ")" ;
 document.getElementById("date").innerHTML = year + "년 " + date_str;
 
-let attend_str = "";
-const listItems = document.getElementById("attend").getElementsByTagName("li");
-console.log(listItems);
-// for(item in listItems) {
-//     console.log(item)
-//     attend_str += item.innerText
-// }
-for (i = 0; i < listItems.length; i++) {
-    //console.log(listItems[i]);
-    console.log(listItems[i].getElementsByClassName("name")[0].innerText)
-    attend_str += listItems[i].getElementsByClassName("name")[0].innerText + ", "
-}
 
-document.getElementById("result-area").innerText = attend_str;
+const listItems = document.getElementById("attend").getElementsByTagName("li");
+// console.log(listItems);
+
+function is_checked()
+{
+    let attend_str = "";
+    let cnt = 0
+    for (i = 0; i < listItems.length; i++) {
+        console.log(listItems[i].getElementsByTagName("input")[0].checked);
+        //console.log(listItems[i].getElementsByClassName("name")[0].innerText)
+        if(listItems[i].getElementsByTagName("input")[0].checked) {
+            cnt++;
+            attend_str += listItems[i].getElementsByClassName("name")[0].innerText + ", "
+        }
+    }
+    attend_str += "(이상 " + cnt + "명)"
+    if(document.getElementById("kancho").checked) {
+        attend_str += "+ 관장님"
+    }
+    document.getElementById("result-area").innerText = attend_str;
+}

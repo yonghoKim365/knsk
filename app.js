4f79850238d13fca31a258bfc5ed17a5340f01b6
ChanBaek7th <7thbaekchan@gmail.com>
2022-09-06 16:38:35 +0900

    이름 표시 등

--- app.js
+++ app.js
@@ -7,5 +7,20 @@ const month = today.getMonth() +1;
 const date = today.getDate();
 const day = today.getDay();
 
-const date_str = month + "월 " + date + "일" + "(" + week[day] + ")" 
-document.getElementById("date").innerHTML = year + "년 " + date_str
+const date_str = month + "월 " + date + "일" + "(" + week[day] + ")" ;
+document.getElementById("date").innerHTML = year + "년 " + date_str;
+
+let attend_str = "";
+const listItems = document.getElementById("attend").getElementsByTagName("li");
+console.log(listItems);
+// for(item in listItems) {
+//     console.log(item)
+//     attend_str += item.innerText
+// }
+for (i = 0; i < listItems.length; i++) {
+    //console.log(listItems[i]);
+    console.log(listItems[i].getElementsByClassName("name")[0].innerText)
+    attend_str += listItems[i].getElementsByClassName("name")[0].innerText + ", "
+}
+
+document.getElementById("result-area").innerText = attend_str;
